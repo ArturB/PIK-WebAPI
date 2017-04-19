fefc0934229f891963f95a7efeb3153c9f2c8cab
ArturB <artur.brodzki@gmail.com>
2017-04-19 11:27:13 +0200

    maven build

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import org.hibernate.HibernateException;
+import org.hibernate.Metamodel;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import pikweb.PointEntity;
+import pikweb.UserEntity;
+import pikweb.Storage;
+
+import java.util.*;
+
+import javax.persistence.metamodel.EntityType;
+
+import java.util.Map;
+
+/**
+ * Created by Artur on 25.03.2017.
+ */
+public class Main {
+
+    public static void main(final String[] args) throws Exception {
+        System.out.println("Main class");
+        Storage storage = new Storage();
+        System.out.println(storage.getAllUsers());
+    }
+
+}
---
+++ src/main/java/pikweb/HomeController.java
@@ -0,0 +1,28 @@
+package pikweb;
+
+import org.hibernate.Hibernate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.*;
+import com.google.gson.*;
+
+
+/**
+ * Created by Artur on 25.03.2017.
+ */
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(value = "/HomeController", method = RequestMethod.GET, produces = "text/plain")
+    @ResponseBody
+    public String getPoints() throws Exception {
+        Storage st = new Storage();
+        Gson g = new Gson();
+        return g.toJson(new Storage().getAllData());
+        //return "HomeController";
+    }
+
+}

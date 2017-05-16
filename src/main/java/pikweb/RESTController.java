1539fa9b2b44f33a2322b7ed85b185cf4a06bdec
ArturB <artur.brodzki@gmail.com>
2017-05-16 10:44:57 +0200

    cross origin enabled

--- src/main/java/pikweb/RESTController.java
+++ src/main/java/pikweb/RESTController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -19,6 +20,7 @@ import java.util.List;
  */
 
 @RestController
+@CrossOrigin
 public class RESTController {
 
     @Autowired
